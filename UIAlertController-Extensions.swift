a4f1901c6314aea6728ab33f62995f75d142369a
noppefoxwolf <noppelabs@gmail.com>
2019-08-28 00:33:57 +0900

    Update UIAlertController-Extensions.swift

--- UIAlertController-Extensions.swift
+++ UIAlertController-Extensions.swift
@@ -24,18 +24,44 @@ extension UIAlertController {
 }
 
 extension UIAlertController {
-    func addDefault(title: String, conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
+    func addDefault(title: String?, conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
         guard conditions else { return }
         addAction(.init(title: title, style: .default, handler: handler))
     }
     
-    func addDestructive(title: String, conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
+    func addDestructive(title: String?, conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
         guard conditions else { return }
         addAction(.init(title: title, style: .destructive, handler: handler))
     }
     
-    func addCancel(title: String = "キャンセル", conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
+    func addCancel(title: String? = "キャンセル", conditions: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
         guard conditions else { return }
         addAction(.init(title: title, style: .cancel, handler: handler))
+    }
+}
+ 
+extension UIAlertController {
+    func addDefault<T>(title: String?, ifExists value: T?, handler: ((UIAlertAction, T) -> Void)? = nil) {
+        if let value = value {
+            addDefault(title: title) { (action) in
+                handler?(action, value)
+            }
+        }
+    }
+    
+    func addDestructive<T>(title: String?, ifExists value: T?, handler: ((UIAlertAction, T) -> Void)? = nil) {
+        if let value = value {
+            addDestructive(title: title) { (action) in
+                handler?(action, value)
+            }
+        }
+    }
+    
+    func addCancel<T>(title: String? = "キャンセル", ifExists value: T?, handler: ((UIAlertAction, T) -> Void)? = nil) {
+        if let value = value {
+            addCancel(title: title) { (action) in
+                handler?(action, value)
+            }
+        }
     }
 }
